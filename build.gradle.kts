420ffe83fe7a1c118e62e290b04b81b034c37558
Mambo <kdevkr@gmail.com>
2025-03-01 00:33:33 +0900

    init: project

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+}
+
+group = "kr.kdev"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("software.amazon.awssdk:bom:2.30.28"))
+    implementation("software.amazon.awssdk:iot")
+    implementation("software.amazon.awssdk:arns")
+    implementation("ch.qos.logback:logback-core:1.5.17")
+    implementation("ch.qos.logback:logback-classic:1.5.17")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
